a7dcd40d75da2a67ef8f281c93157ea575b5d20e
Korotkova Julia <korotkovayuliav@gmail.com>
2022-11-29 13:09:36 +0100

    done

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,38 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> mixedDeque = new ArrayDeque<>();
+        Deque<Integer> newFirstQueue = new LinkedList<>(firstQueue);
+        Deque<Integer> newSecondQueue = new LinkedList<>(secondQueue);
+        if (newFirstQueue.size() >= 2 && newSecondQueue.size() >= 2) {
+            mixedDeque.add(newFirstQueue.poll());
+            mixedDeque.add(newFirstQueue.poll());
+            mixedDeque.add(newSecondQueue.poll());
+            mixedDeque.add(newSecondQueue.poll());
+        }
+        while (!newFirstQueue.isEmpty()) {
+            newFirstQueue.add(mixedDeque.pollLast());
+            mixedDeque.add(newFirstQueue.poll());
+            if (!newFirstQueue.isEmpty()) {
+                mixedDeque.add(newFirstQueue.poll());
+            }
+            newSecondQueue.add(mixedDeque.pollLast());
+            mixedDeque.add(newSecondQueue.poll());
+            if (!newSecondQueue.isEmpty()) {
+                mixedDeque.add(newSecondQueue.poll());
+            }
+        }
+        return mixedDeque;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> firstList = new LinkedList<>(List.of(1, 5, 7, 6));
+        LinkedList<Integer> secondList = new LinkedList<>(List.of(2, 5, 8, 3));
+        //                        List.of(1, 5, 2, 7, 8, 5, 6, 3))
+        ArrayDequeCreator arrayDequeCreator = new ArrayDequeCreator();
+        arrayDequeCreator.createArrayDeque(firstList, secondList);
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,30 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        ArrayDeque<Integer> queue = IntStream.rangeClosed(1, numberOfDishes).boxed()
+                .collect(Collectors.toCollection(ArrayDeque::new));
+        List<Integer> list = new ArrayList<>();
+        int ind = 1;
+        while (!queue.isEmpty()) {
+            if (ind == everyDishNumberToEat) {
+                list.add(queue.remove());
+                ind = 1;
+            } else {
+                ind++;
+                queue.add(queue.remove());
+            }
+        }
+        return list;
+
+    }
+
+    public static void main(String[] args) {
+        DishOrderDeterminer dishOrderDeterminer = new DishOrderDeterminer();
+        System.out.println(dishOrderDeterminer.determineDishOrder(3, 3));
     }
 }
